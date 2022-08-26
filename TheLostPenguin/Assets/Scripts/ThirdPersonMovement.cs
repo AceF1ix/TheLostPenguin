5a3b5ec75d4eea8a774338aaa6ce65e7f19122bb
CubeKR1 <cubekrcube@gmail.com>
2022-08-26 11:01:20 +0200

    Third Person Implementation

---
+++ TheLostPenguin/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    public Transform cam;
+    public float speed = 15f;
+    public float gravity = 9.81f;
+    private float jumpForce = 3f;
+    public float turnSmoothTime = 0.1f;
+    private Animator anim;
+    Vector3 velocity;
+    float turnSmoothVelocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            anim.SetBool("InAir", true);
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * -gravity);
+        }
+        else
+        {
+            anim.SetBool("InAir", false);
+        }
+        
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (direction.magnitude >= 0.5 && isGrounded)
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+    }
+}
